f941c9a57ce60f914fa01e0d6f3fdf8d55bafac9
unknown <adam.day7973@gmail.com>
2023-02-26 14:54:48 +1100

    added formatting to make js work better

--- assets/script.js
+++ assets/script.js
@@ -9,70 +9,58 @@ var passwordConfirmation = '';
 var passwordComplete = '';
 
 function writePassword(){
-  var length = parseInt(prompt("How many characters would you like your password to contain?"))
+  var length = prompt("How many characters would you like your password to contain?")
   if (!length){
-    return; 
-      }
-    } if (length <= 8 || length >= 128) { 
-      alert("Unfortunately, you cannot have a password less than 8 characters, or more than 128 characters.")
-    }
-
-var userNumericChoice = confirm('Click OK to confirm including a Number in your Password');
-var userUpperCaseChoice = confirm('Click OK to confirm including Upper Case Letters in your Password');
-var userLowerCaseChoice = confirm('Click OK to confirm including Lower Case Letters in your Password');
-var userSpecialCharactersChoice = confirm('Click OK to confirm including Special Characters in your Password');
-
-
-    if (userNumericChoice === true) {
-      passwordConfirmation += numericCharacters
-    }
-
-    if (userUpperCaseChoice === true) {
-      passwordConfirmation += userUpperCaseChoice
-    }
-
-    if (userLowerCaseChoice === true) {
-      passwordConfirmation += userLowerCaseChoice
-    }
-    if (userSpecialCharactersChoice === true) {
-      passwordConfirmation += userSpecialCharactersChoice
-    }
-  // adding no value.
-    if (!userNumericChoice || !userUpperCaseChoice || !userLowerCaseChoice || !userSpecialCharactersChoice){
-      passwordConfirmation += ''
-    }
-    if (passwordConfirmation = 0){
-      alert("No password could be generated, please try again")
       return;
-    }
+  }  
+  if (length <= 8 || length >= 128) { 
+      alert("Unfortunately, you cannot have a password less than 8 characters, or more than 128 characters.")
+      return;
+  }
+
+  var passwordLength = parseInt(length);
+
+  var userNumericChoice = confirm('Click OK to confirm including a Number in your Password');
+  var userUpperCaseChoice = confirm('Click OK to confirm including Upper Case Letters in your Password');
+  var userLowerCaseChoice = confirm('Click OK to confirm including Lower Case Letters in your Password');
+  var userSpecialCharactersChoice = confirm('Click OK to confirm including Special Characters in your Password');
+
+  if (userNumericChoice === true) {
+    passwordConfirmation += numericCharacters
+  }
+
+  if (userUpperCaseChoice === true) {
+    passwordConfirmation += upperCaseLetters
+  }
+
+  if (userLowerCaseChoice === true) {
+    passwordConfirmation += lowerCaseLetters
+  }
+  if (userSpecialCharactersChoice === true) {
+    passwordConfirmation += specialCharacters
+  }
+    // adding no value.
+  if (!userNumericChoice || !userUpperCaseChoice || !userLowerCaseChoice || !userSpecialCharactersChoice){
+    passwordConfirmation += ''
+  }
+  if (passwordConfirmation === 0){
+    alert("No password could be generated, please try again")
+  }
 
     // math.floor starting at the beginning of the variable and then randomise; then multiply by length?? gathers number for password
-    for (i = 0; i < length; i++){
-      passwordComplete += passwordConfirmation.charAt(Math.floor(math.random()* passwordConfirmation.length));
-    }
+  for (i = 0; i < passwordLength; i++){
+    passwordComplete += passwordConfirmation.charAt(Math.floor(Math.random() * passwordConfirmation.length));
+    console.log(passwordComplete)
+  }
     
+  var passwordText = document.querySelector("#password");
+  passwordText.value = passwordComplete;
+}   
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+// var password = generatePassword();
 
-  passwordText.value = password;
-
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-    //  this is the var to confirm if user wants lowercased/uppercased characters
-    // var hasLowerCasedCharacters = confirm( 'Click OK to confirm including lowercase characters.');
-    //  this is the var to confirm if user wants lowercased characters
-    // var hasUpperCasedCharacters = confirm( 'Click OK to confirm including Uppercase characters.');
-
-
-// to push a Numeric value after the customer has confirmed they want one if (userResponseToNumeric == true {
-//   password.push(numericCharacters);
-// if (userResponseToNumeric == true {
-//   password.push(numericCharacters);
